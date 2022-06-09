2b2388516b22f9906c9acfa4f76326a72f64a55d
JimLiu35 <gliu35@jhu.edu>
2022-06-09 16:55:12 -0400

    update 02 challenge and 03 challenge cpp files

---
+++ Exercise Files/Chap03/03_Challenge.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+
+struct bookCard {
+	char btitle[50];
+	char bauthor[50];
+	char bpublisher[50];
+	char bsubject[20];
+	char bISBN[11];
+	unsigned long bOCIC;
+	int bDewey[2];
+	int bpubYear;
+	int bacqYear;
+	int bquantity;
+};
+bookCard b1;
+
+int main() {
+	b1.btitle = "The Adventures of Sherlock Holmes";
+	b1.bauthor = "Doyle, Arthur Conan";
+	b1.bpublisher = "George Newnes";
+	b1.bsubject = "Fiction";
+	b1.bISBN = "0393059162";
+	b1.bOCIC = 57490922;
+	b1.bDewey = { 510, 78 };
+	b1.bpubYear = 1982;
+	b1.bacqYear = 2001;
+	b1.bquantity = 4;
+
+	printf("BOOK TITLE: %s\n",b1.btitle);
+	printf("Author: %s\n",b1.bauthor);
+	printf("Publisher: %s\n", b1.bpublisher);
+	printf("Subject: %s\n", b1.bsubject);
+	printf("ISBN: %d\n", b1.bISBN);
+	printf("OCIC: %d\n", b1.bOCIC);
+	printf("Dewey Digits: %d.%d\n", b1.bDewey[0],b1.bDewey[1]);
+	printf("Publish Year: %d\n", b1.bpubYear);
+	printf("Year Acquired: %d\n", b1.bacqYear);
+	printf("Quantity: %d\n", b1.bquantity);
+
+
+	return 0;
+}
